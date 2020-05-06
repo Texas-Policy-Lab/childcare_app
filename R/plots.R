02b9e0cc90bfe6413a8d61cff76c637be633c246
Joli Holmes <jh111@ad.rice.edu>
2020-05-06 15:56:14 -0500

    added covid mapping data to plot

--- R/plots.R
+++ R/plots.R
@@ -18,39 +18,42 @@ theme_map <- function(...) {
 }
 
 map_cbsa <- function(df,
-                     caption = "") {
-  gg <- ggplot(df, aes(x = long, y = lat, group = subregion, fill = fill)) +
-    geom_polygon(fill = "grey",
-                 color = "white") +
-    ggiraph::geom_polygon_interactive(data = df,
-                                      aes(tooltip =  glue::glue("County: {county}<br>Seats: {value}"))
-                                      ) +
+                     covid_df,
+                     caption = "COVID-19 Source: DSHS data reported on 05/06/2020") {
+
+  gg <- ggplot(df, aes(x = long, y = lat)) +
+    ggiraph::geom_polygon_interactive(aes(group = subregion, fill = est, 
+                                            tooltip =  glue::glue("County: {county}<br>Seats: {value}")),
+                                        color = "white"
+                                        ) +
+    geom_point(data = covid_df,
+               aes(x=long, y=lat, size = covid_metric), alpha = .75, color = "#e54e4d") +
     coord_map() +
-    theme_map() + 
-    labs(x = NULL, 
-         y = NULL, 
-         title = NULL, 
-         subtitle = "", 
+    theme_map() +
+    labs(x = NULL,
+         y = NULL,
+         title = NULL,
+         subtitle = "",
          caption = caption) +
     theme(
       plot.title = element_text(hjust = 0.5, color = "#4e4d47"),
-      plot.subtitle = element_text(hjust = 0.5, color = "#4e4d47", 
-                                   margin = margin(b = -0.1, 
-                                                   t = -0.1, 
-                                                   l = 2, 
-                                                   unit = "cm"), 
+      plot.subtitle = element_text(hjust = 0.5, color = "#4e4d47",
+                                   margin = margin(b = -0.1,
+                                                   t = -0.1,
+                                                   l = 2,
+                                                   unit = "cm"),
                                    debug = F),
       plot.margin = unit(c(.5,.5,.2,.5), "cm"),
       panel.spacing = unit(c(-.1,0.2,.2,0.2), "cm"),
       panel.border = element_blank(),
-      plot.caption = element_text(size = 6, 
-                                  hjust = 0.92, 
-                                  margin = margin(t = 0.2, 
-                                                  b = 0, 
-                                                  unit = "cm"), 
+      plot.caption = element_text(size = 6,
+                                  hjust = 0.92,
+                                  margin = margin(t = 0.2,
+                                                  b = 0,
+                                                  unit = "cm"),
                                   color = "#939184")
     ) +
-    scale_fill_manual("Seats per 100", values=c("#e54e4d", "#cea2b2", "#00429d"))
-  
+    scale_fill_manual("Seats per 100", values=c("#ee9391", "#b199bf", "#5c67b1"), na.value = "grey")
+ 
   gg <- ggiraph::girafe(ggobj = gg)
 }
