bba7ddb0b168bc4ef7eb2eb8dac87b5a8b72df72
Joli Holmes <jh111@ad.rice.edu>
2020-05-06 15:55:32 -0500

    added data management steps for the COVID data

--- global.R
+++ global.R
@@ -6,7 +6,6 @@ library(tidyverse)
 library(ggiraph)
 library(maps)
 library(mapproj)
-library(plyr)
 
 rm(list = ls())
 options(encoding = "UTF-8")
@@ -33,7 +32,7 @@ est <- readr::read_csv("./data/Potential_supply_demand_county_5520.csv") %>%
 est_ccs <- est %>% 
   dplyr::select(county, workforce_board, dplyr::starts_with("ccs_per_100")) %>% 
   tidyr::gather(variable, value , -c(county, workforce_board)) %>% 
-  dplyr::mutate(fill = ifelse(value > 35, "> 35",
+  dplyr::mutate(est = ifelse(value > 35, "> 35",
                               ifelse(value <= 35 & value > 15, "> 15 & <= 35", "< 15")),
                 demand = ifelse(grepl("occ", variable), "Occupation", "Industry"),
                 supply = ifelse(grepl("low", variable), "Low",
@@ -49,6 +48,39 @@ est_s <- est %>%
   tidyr::gather(variable, value , -c(county, workforce_board)) %>% 
   dplyr::mutate(variable = gsub(" supply scenario", "", variable))
 
+cases <- readr::read_csv(here::here('./data/covid/confirmed.csv')) %>% 
+  dplyr::select(-Population) %>% 
+  tidyr::gather(variable, `Confirmed cases`, -c(County)) %>% 
+  dplyr::mutate(variable = gsub("Cases", "", variable),
+                variable = gsub("\n", "", variable),
+                variable = paste0(variable, "-2020"),
+                date = lubridate::mdy(variable)) %>% 
+  dplyr::select(-variable) %>% 
+  dplyr::arrange(County, desc(date)) %>% 
+  dplyr::group_by(County) %>% 
+  dplyr::slice(1)
+
+deaths <- readr::read_csv(here::here("./data/covid/deaths.csv")) %>% 
+  dplyr::select(-Population) %>% 
+  tidyr::gather(variable, Deaths, -c(County)) %>% 
+  dplyr::mutate(variable = gsub("Fatalities", "", variable),
+                variable = gsub("\n", "", variable),
+                variable = paste0(variable, "-2020"),
+                date = lubridate::mdy(variable)) %>% 
+  dplyr::select(-variable) %>% 
+  dplyr::arrange(County, desc(date)) %>% 
+  dplyr::group_by(County) %>% 
+  dplyr::slice(1)
+
+covid <- cases %>% 
+  dplyr::left_join(deaths) %>% 
+  dplyr::ungroup() %>% 
+  tidyr::gather(variable, value, -c(County, date)) %>% 
+  dplyr::mutate(County = gsub("\n", " ", County)) %>% 
+  dplyr::select(-date) %>% 
+  dplyr::left_join(wfb %>% 
+                     dplyr::rename(County = county))
+
 tx_counties <- ggplot2::map_data("county", region = "texas") %>% 
   dplyr::mutate(subregion = stringr::str_trim(subregion, "both"),
                 subregion = gsub(" ", "", subregion)) %>% 
@@ -58,96 +90,3 @@ tx_counties <- ggplot2::map_data("county", region = "texas") %>%
                                    subregion = gsub(" ", "", subregion)
                                    )
                    )
-
-# cbsa <- readr::read_csv(here::here("./data/cbsa/cbsa_2013.csv")) %>% 
-#   dplyr::rename(cbsa_cd = `CBSA Code`
-#                ,cbsa_title = `CBSA Title`
-#                ,county = `County/County Equivalent`
-#                ,state = `State Name`
-#                ,state_fips = `FIPS State Code`
-#                ,county_fips = `FIPS County Code`) %>% 
-#   dplyr::select(cbsa_cd, cbsa_title, county, state, state_fips, county_fips) %>% 
-#   dplyr::filter(state_fips == "48")
-# 
-# cbsa_cd <- cbsa %>% 
-#   dplyr::distinct(cbsa_cd, cbsa_title)
-# 
-# cbsa_county <- cbsa %>% 
-#   dplyr::distinct(cbsa_cd, cbsa_title, county, county_fips) %>% 
-#   dplyr::mutate(cbsa = ifelse(is.na(cbsa_cd), 0, 1))
-# 
-# # pop <- readr::read_csv("./data/pop_by_county.csv") %>% 
-# #   dplyr::left_join(cbsa_county) %>% 
-# #   dplyr::mutate(cbsa = ifelse(is.na(cbsa_cd), 0, 1)) %>% 
-# #   dplyr::group_by(cbsa) %>% 
-# #   dplyr::summarise(pop = sum(population)) %>% 
-# #   dplyr::ungroup() %>% 
-# #   dplyr::mutate(total_pop = sum(pop),
-# #                  pct = (pop/total_pop)*100)
-# 
-# txt <- tabulizer::extract_tables("./data/twc_workforce_board.pdf") %>% 
-#   
-# l <- lapply(txt, function(x) {
-#                               x <- as.data.frame(x, stringsAsFactors = FALSE)
-#                               names(x) <- x[1,]
-#                               x <- x[-1,]
-#                               })
-# 
-# df <- do.call("rbind.fill", l)
-# names(df)[1:2] <- c("workforce_board", "county")
-# 
-# df <- df %>% 
-#   dplyr::select(workforce_board, county)
-# 
-# cntys <- stringr::str_trim(unlist(str_split(df$county, ",")), "both")
-# 
-# cntys2 <- sapply(cntys, function(cnty) {
-#   
-#   ifelse(grepl(cnty, df$county), 1, 0)
-#   
-# }, USE.NAMES = TRUE, simplify = FALSE)
-# 
-# cntys3 <- do.call("cbind", cntys2) %>% 
-#   as.data.frame()
-# 
-# df <- df %>% 
-#   dplyr::bind_cols(cntys3) %>% 
-#   dplyr::select(-county) %>% 
-#   tidyr::gather(county, value, -workforce_board) %>% 
-#   dplyr::filter(value == 1) %>% 
-#   dplyr::select(-value)
-#   
-# df <- readr::read_csv("workforce_board_county.csv") %>% 
-#   dplyr::left_join(cbsa_county %>% 
-#                      dplyr::mutate(county = gsub(" County", "", county)) %>% 
-#                      dplyr::filter(cbsa_cd %in% unique(demand$cbsa_cd)))
-# 
-# df_sum <- df %>% 
-#   dplyr::group_by(workforce_board) %>% 
-#   dplyr::summarise(n = sum(cbsa, na.rm = T))
-# 
-# 
-# demand <- readr::read_csv(here::here("./data/Potential_Demand.csv")) %>% 
-#   dplyr::rename(CBSA = MSA
-#                 ,employment_sector = `Parent Employment Sector`
-#                 ,other_child_care_provider = `Other Child Care Provider in the home`
-#                 ,all_children = `All Children (0-11)`
-#                 ,infant_toddler = `Infants and Toddlers (0-2)`
-#                 ,pre_school = `Pre-Schoolers (3-5)`
-#                 ,school_age = `School Age (6-11)`) %>% 
-#   dplyr::filter(state == "Texas") %>% 
-#   dplyr::left_join(cbsa_cd)
-# 
-# assertthat::assert_that(demand %>%
-#                           dplyr::filter(is.na(cbsa_cd)) %>%
-#                           dplyr::distinct(cbsa_title) %>%
-#                           nrow() == 0)
-# 
-# tx_counties <- ggplot2::map_data("county", region = "texas") %>%
-#   dplyr::left_join(cbsa_county %>%
-#                      dplyr::mutate(subregion = tolower(gsub(" County", "", county))) %>%
-#                      dplyr::select(subregion, cbsa_cd, cbsa_title) %>%
-#                      dplyr::filter(cbsa_cd %in% demand$cbsa_cd)) %>%
-#   dplyr::left_join(readr::read_csv(here::here("./data/tx_counties.csv")) %>%
-#                      dplyr::mutate(subregion = tolower(gsub(" County", "", county)))
-#   )
