b70d6939c7a89aeec57610a1f5732f2421f59c8e
Joli Holmes <holmesjoli@gmail.com>
2020-06-09 11:32:07 -0500

    moved demand data management steps to main.R

--- global.R
+++ global.R
@@ -53,24 +53,3 @@ covid <- dm.covid(cases = cases,
                   tx_counties = tx_counties)
 
 map_tx_counties <- dm.tx_counties_map(tx_counties)
-
-pop_data <- dm.pop(pth = config$data$population)
-
-occ_data <- dm.occ_summary(pth = config$data$occ_ind, 
-                           reopen_pct_pth = config$data$occ_reopen,
-                           pop_data = pop_data)
-
-ind_data <- dm.ind_summary(pth = config$data$occ_ind,
-                           reopen_pct_pth = config$data$ind_reopen,
-                           pop_data = pop_data)
-
-df <- occ_data %>% 
-  dplyr::select(Geography, phase3) %>% 
-  dplyr::rename(`Lower Range (Occupation)` = phase3) %>% 
-  dplyr::left_join(
-    ind_data %>% 
-    dplyr::select(Geography, phase3) %>% 
-      dplyr::rename(`Upper Range (Industry)` = phase3)
-  )
-
-write.csv(df, "phase3_reopening.csv", row.names = FALSE)  
